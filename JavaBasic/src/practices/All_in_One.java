e291400d8e8375c1a1243983bd7abe0a2c64a75f
“PushpakPatil17” <patil.pushpak1998@gmail.com>
2022-10-17 00:03:32 +0530

    My New Repo

---
+++ JavaBasic/src/practices/All_in_One.java
@@ -0,0 +1,14 @@
+package practices;
+
+public class All_in_One {
+
+	public static void main(String[] args) {
+	int a=15;
+	int b=20;
+	boolean d;
+	d=b>a;
+	System.out.println("number is : "+(a+b));
+	System.out.println(d);
+	}
+
+}
